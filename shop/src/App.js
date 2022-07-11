d26a1f41ae2d75c723ee279aa0aa341176334a37
Arapov Vasyl <panda.dagon@gmail.com>
2022-07-11 18:35:12 +0300

    css

---
+++ shop/src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+<div className="wrapper">
+  <Header/>
+  <Footer/>
+</div>
+    );
+}
+
+export default App;
