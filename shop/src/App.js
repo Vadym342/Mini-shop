8a6c6db41a3163f2d3a1589271cd8d5d60b2b04b
Arapov Vasyl <panda.dagon@gmail.com>
2022-07-12 19:04:19 +0300

    some css+html

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,49 @@
+import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Items from "./components/Items";
 
-function App() {
-  return (
-<div className="wrapper">
-  <Header/>
-  <Footer/>
-</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "Стіл",
+          img: "table.jpeg",
+          desc: "sometext",
+          category: "1",
+          price: "49.99"
+        },
+        {
+          id: 2,
+          title: "Диван",
+          img: "sofa.jpeg",
+          desc: "sometext",
+          category: "2",
+          price: "51.99"
+        },
+        {
+          id: 3,
+          title: "Тумба",
+          img: "stone.jpeg",
+          desc: "sometext",
+          category: "3",
+          price: "53.99"
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
     );
+  }
 }
 
 export default App;
